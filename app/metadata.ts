a4c1c2b20d39122357b5b5d1e58f06b9f794abd8
Sheena Lindsey-Smith <sheenalindsey@yahoo.com>
2025-05-22 13:27:22 -0400

    Update metadata.ts

--- app/metadata.ts
+++ app/metadata.ts
@@ -2,8 +2,8 @@
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "Achyut Katiyar | Portfolio",
-  description: "Full Stack Developer & Machine Learning Enthusiast",
+  title: "Sheena Lindsey-Smith | Portfolio",
+  description: "AI Engineer & AI Developer ",
   icons: {
     icon: [
       { url: "/favicon.ico", sizes: "any" },
